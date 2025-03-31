cbc95e150304b5ce0278773b2c60376fcb58438e
Tim Taylor <u0671860@utah.edu>
2025-03-31 14:29:43 -0600

    s

--- console/src/main/java/Main.java
+++ console/src/main/java/Main.java
@@ -6,7 +6,7 @@ import valueObjects.Quest;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class ConsoleMain {
+public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 
