548702c7fdc58aae43d06dcb43080858596e7916
Tim Taylor <u0671860@utah.edu>
2025-03-31 13:28:46 -0600

    First Commit

--- domain/src/main/kotlin/constants/LevelConstants.kt
+++ domain/src/main/kotlin/constants/LevelConstants.kt
@@ -3,6 +3,6 @@ package constants
 class LevelConstants {
 	companion object {
 		const val MAX_LEVEL = 100
-		val EXP_TO_LEVEL_MAP = (0..Companion.MAX_LEVEL).associateWith { level -> level * 1000 } //TODO improve exp required to level
+		val EXP_TO_LEVEL_MAP = (0..MAX_LEVEL).associateWith { level -> level * 1000 } //TODO improve exp required to level
 	}
 }
